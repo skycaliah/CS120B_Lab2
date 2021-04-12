89b1f2e405cb353961840afa7e27445e7d9b3cf1
Skyler <sky.saltos2001@gmail.com>
2021-04-11 21:39:08 -0700

    Newly added files for ex 1

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
-/*	Author: lab
+/*	Author: Skyler Saltos 
  *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Lab Section:022
+ *	Assignment: Lab #4  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -13,11 +13,36 @@
 #endif
 
 int main(void) {
-    /* Insert DDR and PORT initializations */
+   	 DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0
+
+	
+       
+
 
     /* Insert your solution below */
     while (1) {
+	
+	char door_sensor = PINA & 0x01;
+        char light_sensor = PINA & 0x02;
+	
+	char lightON = 0x01;
+	char lightOFF = 0x00;	
+	
+	if( (light_sensor == 0x00) && (door_sensor == 0x01) ){
+	
+		PORTB =  lightON;
+		
+	
+	}//dark and door open so light garage
+	
+	else {
 
+		PORTB = lightOFF;
+
+	}//light is not on
+	
     }
     return 1;
 }
+
